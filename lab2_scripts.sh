3d779536625b12ef690e8c007c21c4566ff3ad03
kmetcalfe1 <kmetcalfe1@hotmail.com>
2019-01-30 19:04:35 -0700

    fixed emails

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -17,7 +17,7 @@ grep -c -E ^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$ regex_practice.txt
 
 #problem 2
 echo "number of emails"
-grep -c -E ^\b[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+$ regex_practice.txt
+grep -c -E ^[A-Za-z0-9._%+-]+@[A-Za-z.]+\.[A-Za-z]+$ regex_practice.txt
 
 #problem 3
 echo "all 303 numbers"
@@ -25,7 +25,7 @@ grep -E ^303[-][0-9]{3}[-][0-9]{4}$ regex_practice.txt > phone_results.txt
 
 #problem 4
 echo "all emails and storing"
-grep -E ^[A-Za-z]+@geocities.com$ regex_practice.txt > email.results.txt
+grep -E ^[A-Za-z0-9]+@geocities.com$ regex_practice.txt > email.results.txt
 
 #problem 5
 echo "enter regex now"
